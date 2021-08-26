5f1d9ddadcbc6b93e83f65adebba312eeb95c14f
Seonghwa Yun <seonghwa.yun@comcom.ai>
2021-08-26 16:05:07 +0900

    update port

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import express from 'express';
 import heRouter from './api/he';
 
 const app = express();
-const port = 17999;
+const port = 3000;
 
 app.use(express.json({ limit: '5mb' }));
 app.use(express.urlencoded({ 
