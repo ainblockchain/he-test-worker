776d851ff78f1b5b4120a6ffe4db6c2802aeac19
Seonghwa Yun <seonghwa.yun@comcom.ai>
2021-09-14 16:50:00 +0900

    fix default port

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import express from 'express';
 import heRouter from './api/he';
 
 const app = express();
-const port = 3000;
+const port = process.env.NODE_PORT || 5000;
 
 app.use(express.json({ limit: '5mb' }));
 app.use(express.urlencoded({ 
