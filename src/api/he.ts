b58e3f17c1dba384615a44e779cc9fbea5c5784f
Seonghwa Yun <seonghwa.yun@comcom.ai>
2021-11-16 18:43:50 +0900

    fix ainJs init logic

--- src/api/he.ts
+++ src/api/he.ts
@@ -4,5 +4,6 @@ import * as Handler from '../apiHandler/he'
 const router = express.Router();
 
 router.post('/request', Handler.request);
+// router.post('/cardrisk', Handler.cardRisk);
 
 export default router;
--- src/apiHandler/he.ts
+++ src/apiHandler/he.ts
@@ -1,8 +1,14 @@
 import express from 'express';
 import axios from 'axios';
 import { StatusCodes } from 'http-status-codes';
+import AinJs from '@ainblockchain/ain-js';
+import { TransactionInput } from '@ainblockchain/ain-js/lib/types';
 
-import { NODE_URL } from '../common/constants';
+import {
+  NODE_URL,
+  MNEMONIC_WORDS,
+  DEFAULT_HE_PARAMS
+} from '../common/constants';
 
 const requestList: string[] = [];
 
@@ -15,40 +21,40 @@ const getEncryptStrFromBlockchain = async (path: string, nodeSeal: any) => {
   return cipherText;
 }
 
-const encodePlain = (he: any, value: number, cipherText: any) => {
+const encodePlain = (he: any, value: number, scale: any, parmsId: any) => {
   const target = Float64Array.from([value]);
-  let plainText = he.seal.encoder.encode(target, cipherText.scale);
-  plainText = he.seal.evaluator.plainModSwitchTo(plainText, cipherText.parmsId);
+  let plainText = he.seal.encoder.encode(target, scale);
+  plainText = he.seal.evaluator.plainModSwitchTo(plainText, parmsId);
   return plainText;
 }
 
 const getCardRisk = async (he: any, value: any) => {
   const {
-    op1, /* Total Cholesterol */
-    op2, /* Triglyceride */
-    op3, /* HDL-Cholesterol */
+    op1Path, /* Total Cholesterol */
+    op2Path, /* Triglyceride */
+    op3Path, /* HDL-Cholesterol */
   } = value;
-  const { evaluator, context, seal } = he.seal;
+  const { evaluator, context } = he.seal;
 
   const coeff = [ 0.72976753, -0.31200519, 0.00335608 ];
   const intercept = -12.604077534530276;
 
-  const ciphers = [seal.CipherText(), seal.CipherText(), seal.CipherText()];
-  ciphers[0].load(context, op1);
-  ciphers[1].load(context, op2);
-  ciphers[2].load(context, op3);
+  const ciphers = new Array<any>(3);
+  ciphers[0] = await getEncryptStrFromBlockchain(op1Path, he.seal);
+  ciphers[1] = await getEncryptStrFromBlockchain(op2Path, he.seal);
+  ciphers[2] = await getEncryptStrFromBlockchain(op3Path, he.seal);
 
   let enc_result = evaluator.multiplyPlain(ciphers[0],
-    encodePlain(he, coeff[0], ciphers[0]));
+    encodePlain(he, coeff[0], ciphers[0].scale, ciphers[0].parmsId));
   enc_result = evaluator.rescaleToNext(enc_result);
   for (let i = 1; i < ciphers.length; i = i + 1) {
     let enc_tmp = evaluator.multiplyPlain(ciphers[i],
-      encodePlain(he, coeff[i], ciphers[i]));
+      encodePlain(he, coeff[i], ciphers[i].scale, ciphers[0].parmsId));
     enc_tmp = evaluator.rescaleToNext(enc_tmp);
     enc_result = evaluator.add(enc_result, enc_tmp);
   }
   enc_result = evaluator.addPlain(enc_result,
-    encodePlain(he, intercept, enc_result));
+    encodePlain(he, intercept, enc_result.scale, enc_result.parmsId));
 
   const result = enc_result.save();
   return result;
@@ -61,8 +67,8 @@ const getFallRisk = async (he: any, value: any) => {
   } = value;
   const { seal: nodeSeal, evaluator } = he;
 
-  const cOp1 = getEncryptStrFromBlockchain(op1Path, nodeSeal);
-  const cOp2 = getEncryptStrFromBlockchain(op2Path, nodeSeal);
+  const cOp1 = await getEncryptStrFromBlockchain(op1Path, nodeSeal);
+  const cOp2 = await getEncryptStrFromBlockchain(op2Path, nodeSeal);
 
   let enc_result = evaluator.add(cOp1, cOp2);
 
@@ -75,13 +81,16 @@ export const request = async (
   res: express.Response,
 ) => {
   try {
-    const ainJs = res.locals.ainJs;
+    const ainJs = new AinJs(NODE_URL);
+    const address = ainJs.wallet.addFromHDWallet(MNEMONIC_WORDS);
+    ainJs.wallet.setDefaultAccount(address);
+    await ainJs.he.init(null, DEFAULT_HE_PARAMS);
+
     const { function: func, transaction } = req.body;
     // TODO: check transaction signature?
     const { ref, value } = transaction.tx_body.operation;
     const parsedRef = ref.split('/');
     const requestId = parsedRef[6];
-    console.log(`[+] requestId: ${requestId}`);
     if (requestList.includes(requestId)) {
       res.sendStatus(200);
       return;
@@ -89,7 +98,7 @@ export const request = async (
       requestList.push(requestId);
     }
     console.log(`[+] Request received: ${ref}`)
-    // console.log(`[+] payload: ${JSON.stringify(value)}`);
+    console.log(`[+] payload: ${JSON.stringify(value)}`);
 
     let result;
     switch (value.type) {
@@ -109,7 +118,7 @@ export const request = async (
 
 		console.log(`[+] Result for ${requestId}: ${result.substring(0, 15)}...`);
 		const resultRef = `/apps/he_health_care/tasks/response/${value.user_address}/${requestId}`;
-    const tx = {
+    const tx: TransactionInput = {
       operation: {
         type: 'SET_VALUE',
         ref: resultRef,
@@ -141,3 +150,47 @@ export const request = async (
     });
   }
 }
+
+/* 
+export const cardRisk = async (
+  req: express.Request,
+  res: express.Response,
+) => {
+  try {
+    const he = res.locals.ainJs.he;
+    const { op1, op2, op3 } = req.body;
+    const { evaluator, context, seal } = he.seal;
+    console.log('[+] Request received: cardRisk');
+    // console.log(`[+] payload: ${JSON.stringify(req.body)}`);
+
+    const coeff = [ 0.72976753, -0.31200519, 0.00335608 ];
+    const intercept = -12.604077534530276;
+
+    const ciphers = [seal.CipherText(), seal.CipherText(), seal.CipherText()];
+    ciphers[0].load(context, op1);
+    ciphers[1].load(context, op2);
+    ciphers[2].load(context, op3);
+
+    let enc_result = evaluator.multiplyPlain(ciphers[0],
+      encodePlain(he, coeff[0], ciphers[0].scale, ciphers[0].parmsId));
+    enc_result = evaluator.rescaleToNext(enc_result);
+    for (let i = 1; i < ciphers.length; i = i + 1) {
+      let enc_tmp = evaluator.multiplyPlain(ciphers[i],
+        encodePlain(he, coeff[i], ciphers[i].scale, ciphers[i].parmsId));
+      enc_tmp = evaluator.rescaleToNext(enc_tmp);
+      enc_result = evaluator.add(enc_result, enc_tmp);
+    }
+    enc_result = evaluator.addPlain(enc_result,
+      encodePlain(he, intercept, enc_result.scale, enc_result.parmsId));
+
+    const result = enc_result.save();
+    console.log(`[+] Result for cardRisk: ${result.substring(0, 15)}...`);
+    res.status(StatusCodes.OK).json({ result });
+  } catch (e: any) {
+    console.log(`[-] Internal error: ${e.message}`);
+    res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
+      message: e.message,
+    });
+  }
+}
+*/
